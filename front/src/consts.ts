6027a536b5d7618b5a171187d893b8afda5c92f4
radlinskii <radlinskiignacy@gmail.com>
2024-11-05 08:03:54 +0100

    update metadata

--- front/src/consts.ts
+++ front/src/consts.ts
@@ -1,6 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "radlinski.dev";
-export const SITE_DESCRIPTION = "some links and stuff";
+export const SITE_TITLE = "Ignacy Radliński";
+export const SITE_DESCRIPTION =
+    "Software Engineer skilled in full-stack web and desktop application development, focused on enhancing user and developer experience, refining workflows, and tinkering with ergonomic mechanical keyboards.";
 export const SITE_IMAGE = "/background_switzerland_1200w.jpeg";
