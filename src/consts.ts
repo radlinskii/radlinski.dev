0f6ba6c2f3ad1770ecae05db28021108f2f5f6ea
radlinskii <radlinskiignacy@gmail.com>
2024-11-07 18:30:14 +0100

    fix: fix home page link in header

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Ignacy Radliński";
+export const SITE_NAME = "radlinski.dev";
 export const SITE_DESCRIPTION =
     "Software Engineer skilled in full-stack web and desktop application development, focused on enhancing user and developer experience, refining workflows, and tinkering with ergonomic mechanical keyboards.";
 export const SITE_IMAGE = "/background_switzerland_1200w.jpeg";
