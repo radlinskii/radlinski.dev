acd9e3b08d19f5863c61cb72fd63fedeec5265f8
radlinskii <radlinskiignacy@gmail.com>
2026-07-06 13:08:31 +0200

    update astro by major version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import { unified } from "@astrojs/markdown-remark";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://radlinski.dev",
-    integrations: [mdx(), sitemap()],
+    integrations: [mdx({ processor: unified() }), sitemap()],
 });
