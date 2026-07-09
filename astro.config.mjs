4d82d6f7952b7832ff357620134f86da5014598c
radlinskii <radlinskiignacy@gmail.com>
2026-07-09 21:57:21 +0200

    header links plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,23 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import { unified } from "@astrojs/markdown-remark";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://radlinski.dev",
-    integrations: [mdx({ processor: unified() }), sitemap()],
+    integrations: [
+        mdx({
+            processor: unified({
+                rehypePlugins: [
+                    rehypeSlug,
+                    [rehypeAutolinkHeadings, { behavior: "wrap" }],
+                ],
+            }),
+        }),
+        sitemap(),
+    ],
 });
